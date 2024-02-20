cd7cdec4ae3a98937c1e19ddeda15126550cf830
Natt-s <Natthaphon.s2018@gmail.com>
2024-02-20 18:05:46 +0700

    demo 1.0

--- scripts/hook.gd
+++ scripts/hook.gd
@@ -1,6 +1,10 @@
-extends RigidBody2D
+extends Area2D
+class_name Hook
 
+@export var speed = 2000
+@export var direction = Vector2(0,0)
 
+signal wall_hit(position)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +13,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+func _physics_process(delta):
+	position += direction * speed * delta
+
+
+
+
+func _on_body_entered(body):
+	if body is StaticBody2D:
+		wall_hit.emit(position)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,20 @@
 extends CharacterBody2D
 class_name Player
 signal hit
-signal shoot
+signal shoot(direction)
 
 @export var speed = 400
 @export var jump_force = -450 # Usually jump force should be negative
 @export var gravity = 980 # Adjust the gravity to your needs
+@export var hook_speed = 2000
 var screen_size # Size of the game window.
 var jump_state
 var jump_quota = 3
 var state = 'normal'
-var clickPos
 
 func start(pos):
 	position = pos
+	state = 'normal'
 	show()
 	$CollisionShape2D.disabled = false
 
@@ -21,43 +22,52 @@ func start(pos):
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
-	
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed:
-			print(event.position)
-			clickPos = event.position
 		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	velocity.x = 0
-	velocity.y += gravity*delta
+	#print(state)
+	if state == "dead": return
 	
 	if Input.is_action_just_pressed("shoot"):
-		shoot.emit(clickPos)
-		print("shoot")
+		var cliclPos = get_local_mouse_position()
+		var direction = cliclPos.normalized()
+		shoot.emit(direction)
 	
-	if is_on_floor():
-		state = 'normal'
-		jump_state = 0
-	if state == 'normal' and Input.is_action_pressed("move_right"):
-		velocity.x = speed
-	if state == 'normal' and Input.is_action_pressed("move_left"):
-		velocity.x = -speed
+	if state == 'normal':
+		velocity.x = 0
+		velocity.y += gravity*delta
+		if is_on_floor():
+			state = 'normal'
+			jump_state = 0
+		if Input.is_action_pressed("move_right"):
+			velocity.x = speed
+		if Input.is_action_pressed("move_left"):
+			velocity.x = -speed
+	elif state == "just_hooked":
+		state = "hooking"
+
+		
+	
 	if Input.is_action_just_pressed("jump") and jump_state < jump_quota:
 		state = 'normal'
 		jump_state += 1
 		velocity.y = jump_force
-	#position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, screen_size)
+		#position += velocity * delta
+		#position = position.clamp(Vector2.ZERO, screen_size)
 	move_and_slide()
+
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		if collision.get_collider() is Enemy:
 			hide()
 			hit.emit()
+			state = "dead"
 			$CollisionShape2D.set_deferred("disabled", true)
+		elif state == "hooking":
+			state = "normal"
+			velocity.y = 0
+			
 	
 	if velocity.x != 0 and velocity.y != 0:
 		$AnimatedSprite2D.play()
@@ -72,6 +82,13 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 	pass
 
-#func shoot():
-	#pass
+func _on_wall_hooked(position):
+	print("on wall hooked", position)
+	if state == "dead": return
+	
+	var direction = (position - self.position).normalized()
+	#print(direction)
+	state = "just_hooked"
+	set_velocity(direction * 2000)
+	
 
