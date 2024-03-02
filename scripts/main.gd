339537404ac08f99e6fc5ed85f3d35f70d548bdd
Natthaphon Suphansri <Natthaphon.s2018@gmail.com>
2024-03-02 15:18:54 +0700

    fix: re-enable mob spawning

--- scripts/main.gd
+++ scripts/main.gd
@@ -76,7 +76,7 @@ func _on_score_timer_timeout():
 	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
-	#$MobTimer.start()
+	$MobTimer.start()
 	$ScoreTimer.start()
 	print("timer start")
 
