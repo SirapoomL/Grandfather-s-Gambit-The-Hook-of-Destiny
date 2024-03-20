8b63f8f406e6871982ef48a029351ad21e9d3859
Bhuribhat <Bhuribhat@gmail.com>
2024-03-20 11:08:50 +0700

    feat: spirit levitate mechanic

--- scripts/ground_enemy_spirit.gd
+++ scripts/ground_enemy_spirit.gd
@@ -9,6 +9,8 @@ var player = null
 var speed = 60
 var hp = 50
 var exp = 10
+
+# NOTE Set GRAVITY to false to make spirit levitate toward player
 var GRAVITY = true
 
 # Distance between player and mob for attack range
@@ -21,28 +23,29 @@ func _physics_process(delta):
 		global_position.y += 980 * delta
 	if player_chase:
 		if not _check_in_attack_range(left_dir_range, right_dir_range):
+			$AnimatedSprite2D.play("spirit_move")
+			
+			# Move spirit in x-axis
 			if (player.position.x > position.x):
 				position.x += speed * delta
 			else:
 				position.x -= speed * delta
 				
-			# Make spirit fly towards player
+			# Make spirit levitating towards player
 			if not GRAVITY && (player.position.y > position.y):
 				position.y += speed * delta
 			elif not GRAVITY && (player.position.y < position.y):
 				position.y -= speed * delta
 			
-			$AnimatedSprite2D.play("spirit_move")
-			
 			# Flip spirit face direction
-			if (player.position.x - position.x) < 0:
+			if (player.position.x - position.x) < 5:
 				$AnimatedSprite2D.flip_h = true
-			elif (player.position.x - position.x) > 0:
+			elif (player.position.x - position.x) > 5:
 				$AnimatedSprite2D.flip_h = false
 
-		# Spirit in attack range (stop spirit movement)
+		# Player is in spirit's attack range (stop spirit movement)
 		else:
-			## TODO attack player function
+			# TODO attack player function
 			$AnimatedSprite2D.play("spirit_attack")
 	else:
 		$AnimatedSprite2D.play("spirit_idle")
@@ -70,8 +73,8 @@ func _check_in_attack_range(left_dir_range, right_dir_range):
 	return player_chase && \
 	not ((player.position.x - position.x) < left_dir_range || (player.position.x - position.x) > right_dir_range)  && \
 	not ((player.position.y - position.y) < left_dir_range || (player.position.y - position.y) > right_dir_range)
-	
-	
+
+
 func hit(damage, knockback=30):
 	# Add knockback when getting hit
 	if (player.position.x > position.x):
@@ -79,12 +82,12 @@ func hit(damage, knockback=30):
 	else:
 		position.x += knockback
 	
+	# Decrease hitpoint and popping text
 	hp -= damage
 	var text = floating_text.instantiate()
 	text.amount = damage
 	add_child(text)
 
-	#get_tree().root.add_child(text)
 	if hp <= 0:
 		return [damage, exp]
 	return [damage, 0]
